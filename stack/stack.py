3cdaca7235fc1b3b2c97368b7cb1024402ff12d1
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-07-13 14:58:17 +0530

    some stack operations in python

---
+++ stack/stack.py
@@ -0,0 +1,35 @@
+"""
+Stack Data Structure.
+"""
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)				
+
+    def pop(self):
+        return self.items.pop()
+
+    def get_stack(self):
+        return self.items
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+myStack = Stack()
+print(myStack.is_empty())
+myStack.push("A")
+myStack.push("B")
+print(myStack.get_stack())
+myStack.push("C")
+print(myStack.get_stack())
+myStack.pop()
+print(myStack.get_stack())
+print(myStack.is_empty())
+print(myStack.peek())
+print(myStack.get_stack())
