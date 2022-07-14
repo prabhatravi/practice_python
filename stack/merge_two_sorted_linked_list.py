a678b337e3b3ded141e6c21d20d7baaa64fbab5c
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-07-14 18:06:50 +0530

    Remove duplicates from single linked list

--- stack/merge_two_sorted_linked_list.py
+++ stack/merge_two_sorted_linked_list.py
@@ -205,6 +205,24 @@ class LinkedList:
         self.head = new_head     
         return self.head
 
+    def remove_duplicates(self):
+            
+        cur = self.head
+        prev = None
+
+        dup_values = dict()
+
+        while cur:
+            if cur.data in dup_values:
+                # Remove node:
+                prev.next = cur.next
+                cur = None
+            else:
+                # Have not encountered element before.
+                dup_values[cur.data] = 1
+                prev = cur
+            cur = prev.next
+
 llist_1 = LinkedList()
 llist_2 = LinkedList()
 
