e36a12d1dea7c045014b2d6588273847d1ea3e77
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-07-18 19:15:39 +0530

    Added add  a new node after/before a particular node

--- doubly linked list/doubly_ll.py
+++ doubly linked list/doubly_ll.py
@@ -36,6 +36,39 @@ class DoublyLinkedList:
             new_node.next = self.head
             self.head = new_node
 
+    def add_after_node(self, key, data):
+        cur = self.head
+        while cur:
+            if cur.next is None and cur.data == key:
+                self.append(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                nxt = cur.next
+                new_node.prev = cur
+                new_node.next = nxt
+                cur.next = new_node
+                nxt.prev = new_node
+                return
+            cur = cur.next
+
+    def add_before_node(self, key, data):
+        cur = self.head
+        while cur:
+            if cur.prev is None and cur.data == key:
+                self.prepend(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                prev = cur.prev
+                cur.prev = new_node
+                new_node.next = cur
+                new_node.prev = prev
+                prev.next = new_node
+                return
+            cur = cur.next
+
+
 dllist = DoublyLinkedList()
 dllist.prepend(0)
 dllist.append(1)
@@ -43,5 +76,7 @@ dllist.append(2)
 dllist.append(3)
 dllist.append(4)
 dllist.prepend(5)
+dllist.add_after_node(4, 6)
+dllist.add_before_node(1, 7)
 
 dllist.print_list()
